437ac2c8f2446c2a95f503798c30d8f64799380f
69070188 <69070188@kmitl.ac.th>
2026-08-27 20:36:43 +0700

    Add files via upload

---
+++ oj3072/AEIOU1.py
@@ -0,0 +1,15 @@
+"""AEIOU"""
+def main():
+    """AEIOU"""
+    aeiou = input().lower()
+    if aeiou.count('a'):
+        print(f"a : {aeiou.count('a')}")
+    if aeiou.count('e'):
+        print(f"e : {aeiou.count('e')}")
+    if aeiou.count('i'):
+        print(f"i : {aeiou.count('i')}")
+    if aeiou.count('o'):
+        print(f"o : {aeiou.count('o')}")
+    if aeiou.count('u'):
+        print(f"u : {aeiou.count('u')}")
+main()

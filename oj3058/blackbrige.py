68f7e9c04f74ada5a633a3b685c2e78d8f1582ef
69070188 <69070188@kmitl.ac.th>
2026-08-27 20:25:49 +0700

    Add files via upload

---
+++ oj3058/blackbrige.py
@@ -0,0 +1,15 @@
+"""blackbrige"""
+def main():
+    """blackbrige"""
+    a = int(input())
+    b = int(input())
+    goal = int(input())
+
+    use_big = min(b, goal // 5)
+    remaining_length = goal - (use_big * 5)
+
+    if a >= remaining_length:
+        print(remaining_length)
+    else:
+        print(-1)
+main()
